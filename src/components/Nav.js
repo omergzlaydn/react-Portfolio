69576761e413cd5e7219ac770e6f9aa40735bb2c
omergzlaydn <omrgzlaydn@gmail.com>
2024-03-21 01:08:57 +0300

    -

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,18 +7,18 @@ const Nav = () => {
   return (
     <nav className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50 '>
       <div className='container mx-auto'>
-        <div className='w-full bg-black/20 h-[96px] backdrop-blur-2x1 rounded-full max-w-[460px] mx-auto px-5 flex justify-between text-2x1 text-white/50'>
-          <Link to='section1' smooth={true} duration={500}>
+        <div className='w-full bg-black/20 h-[96px] backdrop-blur-2x1 rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2x1 text-white/50'>
+          <Link clossName='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
             <BiHomeAlt/>
           </Link>
-          <Link to='section2' smooth={true} duration={500}>
+          <Link clossName='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+            <BiUser/>
+          </Link>
+          <Link clossName='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
             <BsClipboardData/>
           </Link>
-          <Link to='section3' smooth={true} duration={500}>
+          <Link clossName='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
             <BsBriefcase/>
-          </Link>
-          <Link to='section4' smooth={true} duration={500}>
-            <BsChatSquare/>
           </Link>
         </div>
       </div>
