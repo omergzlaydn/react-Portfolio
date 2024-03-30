92201603ac538e6e5f6f0c1817eb27f6fbc89ad0
omergzlaydn <omrgzlaydn@gmail.com>
2024-03-31 02:25:59 +0300

    -

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,84 @@
 import React from 'react';
+import CountUp from 'react-countup';
+import { useInView } from 'react-intersection-observer';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants';
+import { FaLinkedin, FaGithub, FaInstagram, FaYoutube } from 'react-icons/fa'; // Iconları import ettik
 
 const About = () => {
-  return <div className='section' id='about'>About</div>;
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
+
+  return (
+    <section className='section' id='about' ref={ref}>
+      <div className='container mx-auto'>
+        <div className='flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top'>
+          img
+        </div>
+        <motion.div
+          variants={fadeIn}
+          initial='hidden'
+          animate={inView ? 'show' : 'hidden'}
+          className='text-center mt-12'
+        >
+          <h2 className='text-3xl lg:text-5xl font-bold mb-6'>About Me</h2>
+          <p className='text-lg lg:text-xl leading-relaxed'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+            efficitur mi ut erat eleifend dapibus. Nulla facilisi. Integer nec
+            fermentum ex, in rutrum sapien. Vivamus tincidunt sapien sed
+            lectus sagittis, a consequat enim ultrices. Phasellus posuere
+            aliquam metus, id accumsan velit pharetra vel. Sed lobortis justo
+            sit amet orci venenatis, at molestie turpis consectetur.
+          </p>
+          <div className='mt-8'>
+            <p className='text-xl font-semibold'>Years of Experience:</p>
+            <motion.div variants={fadeIn} className='text-3xl lg:text-5xl'>
+              <CountUp start={0} end={5} duration={2.5} suffix='+' />
+            </motion.div>
+          </div>
+        </motion.div>
+        <motion.div
+          variants={fadeIn("up", 0.6)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          whileInView={{ show: { opacity: 1 } }}
+          viewport={{ once: false, threshold: 0.7 }}
+          className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0 justify-center" // Ortalamak için justify-center sınıfı eklendi
+        >
+          <button className="btn btn-lg mb-4 lg:mb-0">contact me</button>
+        </motion.div>
+        <motion.div
+          variants={fadeIn("up", 0.6)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          whileInView={{ show: { opacity: 1 } }}
+          viewport={{ once: false, threshold: 0.7 }}
+          className="flex flex-col lg:flex-row items-center gap-y-10 lg:gap-y-0 lg:gap-x-6 lg:justify-start mb-12 mx-auto lg:mx-0" // flex-col, gap-y-10 ve lg-flex-row sınıfları eklendi
+        >
+          <a href="#" className="text-gradient btn-link mr-4">
+            My Portfolio
+          </a>
+          <div className="flex items-center gap-x-6">
+            <a href="#">
+              <FaLinkedin />
+            </a>
+            <a href="#">
+              <FaGithub />
+            </a>
+            <a href="#">
+              <FaInstagram />
+            </a>
+            <a href="#">
+              <FaYoutube />
+            </a>
+          </div>
+        </motion.div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -13,28 +13,77 @@ import Image from "../assets/avatar.svg";
 
 const Banner = () => {
   return (
-    <section className="min-h-[85vh] lg:min-h-[78vh] flex items-center" id="home">
+    <section
+      className="min-h-[85vh] lg:min-h-[78vh] flex items-center"
+      id="home"
+    >
       <div className="container mx-auto">
         <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
           <div className="flex-1 text-center font-secondary lg:text-left">
-            <h1 className="mb-6 text-[46px] font-bold leading-[0.8] lg:text-[65px]">
-              Ömer{" "}
-              <span className="text-white mb-6 text-[70px]">GÜZELAYDIN</span>
-            </h1>
-            <div className="text-accent mb-6 text-[36px] lg-text[60px] font-secondary font-semibold uppercase leading-[1]">
+            <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              whileInView={{ show: { opacity: 1 } }}
+              viewport={{ once: false, threshold: 0.7 }}
+              className="text-[55px] font-bold leading-[0.8] lg:text-[55px] text-center"
+            >
+              ÖMER{" "}
+              <span className="text-[#ffffff] mb-6 text-[55px]">
+                GÜZELAYDIN
+              </span>
+            </motion.h1>
+
+            <motion.div
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              whileInView={{ show: { opacity: 1 } }}
+              viewport={{ once: false, threshold: 0.7 }}
+              className="text-accent mb-6 text-[46px] lg-text[60px] font-secondary font-semibold uppercase leading-[1]"
+            >
               <span className="mr-4">I am a </span>
               <TypeAnimation
-                sequence={["Frontend", 2000, "Mobile", 2000, "Developer", 1000]}
+                sequence={[
+                  "Frontend Dev.",
+                  2000,
+                  "Mobile Dev.",
+                  2000,
+                  "SoftwareDev.",
+                  1000,
+                ]}
                 speed={25}
                 wrapper="span"
                 repeat={Infinity}
               />
-            </div>
-            <p className="mb-8 max-w-lg mx-auto lg:mx-0">Lorem ipsum</p>
-            <div className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
+            </motion.div>
+            <motion.p
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              whileInView={{ show: { opacity: 1 } }}
+              viewport={{ once: false, threshold: 0.7 }}
+              className="mb-8 max-w-lg mx-auto lg:mx-0"
+            >
+              Lorem ipsum
+            </motion.p>
+            <motion.div
+              variants={fadeIn("up", 0.6)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              whileInView={{ show: { opacity: 1 } }}
+              viewport={{ once: false, threshold: 0.7 }}
+              className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0"
+            >
               <button className="btn btn-lg mb-4 lg:mb-0">contact me</button>
               <div className="flex flex-row items-center justify-center lg:justify-start">
-                <a href="#" className="text-gradient btn-link mr-4">My Portfolio</a>
+                <a href="#" className="text-gradient btn-link mr-4">
+                  My Portfolio
+                </a>
                 <div className="flex items-center gap-x-6">
                   <a href="#">
                     <FaLinkedin />
@@ -50,11 +99,16 @@ const Banner = () => {
                   </a>
                 </div>
               </div>
-            </div>
+            </motion.div>
           </div>
-          <div className="hidden lg:flex flex-1 max-w-[380px] lg:max-w-[542px]">
+          <motion.div
+            variants={fadeIn("down", 0.5)}
+            initial="hidden"
+            animate="show"
+            className="hidden lg:flex flex-1 max-w-[380px] lg:max-w-[542px]"
+          >
             <img src={Image} alt="Avatar" />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
